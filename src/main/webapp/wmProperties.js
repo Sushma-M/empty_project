7a8657395b2452f156e5a12e01014d9f64d62f2c
Sushma Maddirala <sushma.maddirala@imaginea.com>
2017-09-16 09:09:39 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "empty project",
+  "homePage" : "Main",
+  "name" : "empty_project",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
