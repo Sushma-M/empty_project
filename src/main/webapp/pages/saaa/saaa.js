0b75deb15b9cc35eeb11371d868e3d10e054e1c2
Sushma Maddirala <sushma.maddirala@imaginea.com>
2017-09-16 12:25:34 +0000

    s New files added - [saaa] Files modified - [webapp,pom.xml,hrdb.properties,pages-config.json]

---
+++ src/main/webapp/pages/saaa/saaa.js
@@ -0,0 +1,10 @@
+Application.$controller("saaaPageController", ["$scope", function ($scope) {
+    "use strict";
+}]);
+
+Application.$controller("grid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
